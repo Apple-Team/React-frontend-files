fcb80568154d51419f87ab26b7bf021a6465f6f7
vsaisruthi <vsaisruthi@Sruthi.local>
2017-02-25 16:24:43 +0530

    merge

--- Reactcode/src/UpdateRest.js
+++ Reactcode/src/UpdateRest.js
@@ -3,7 +3,7 @@ import {Link,hashHistory} from 'react-router';
 import AdminHeader from './AdminHeader';
 // webpack.config.js specifies index.js as the entry point, and
 // index.js imports and renders this `App` component.
-
+var lat,lon;
 class UpdateRest extends Component {
   constructor() {
     // In a constructor, call `super` first if the className extends another className
@@ -12,12 +12,25 @@ class UpdateRest extends Component {
     this.state = {
     data1: [],
     get_data: [],
-    image_data:[]
+    image_data:[],
+    lat1:[],lon1:[]
   };
       this.handleUpload = this.handleUpload.bind(this);
-
+      this.handleUpdateMap = this.handleUpdateMap.bind(this);
+      this.getLoc = this.getLoc.bind(this);
      console.log('test');
   }
+  componentWillMount(){
+  fetch("http://localhost:9000/restaurants_by_id/"+this.props.params.index)
+         .then((response) => response.json())
+         .then((responseJson) => {
+            this.setState({
+             get_data: responseJson
+
+            });
+         });
+}
+
   handleUpload(){
       var formData = new FormData();
       var photo=document.getElementById('FileUpload').files[0];
@@ -37,19 +50,113 @@ class UpdateRest extends Component {
       });
 
   }
-
-
-  componentWillMount(){
-  fetch("http://localhost:9000/restaurants_by_id/"+this.props.params.index)
-         .then((response) => response.json())
-         .then((responseJson) => {
-            this.setState({
-             get_data: responseJson
-
-            });
+  handleUpdateMap() {
+         var map = new google.maps.Map(document.getElementById('map'), {
+           center: {lat: 17.3850, lng: 78.4867},
+           zoom: 13,
+           panControl:true,
+          zoomControl:true,
+          mapTypeControl:true,
+          scaleControl:true,
+          streetViewControl:true,
+          overviewMapControl:true,
+          rotateControl:true
          });
-  console.log(this.state.get_data);
-}
+
+
+         // Create the search box and link it to the UI element.
+         var input = document.getElementById('pac-input');
+         console.log(input.value);
+
+
+          map.addListener('click', function(e) {
+            placeMarkerAndPanTo(e.latLng, map);
+          });
+
+         var marker;
+       function placeMarkerAndPanTo(latLng, map) {
+         if(null==marker){
+
+           marker = new google.maps.Marker({
+           position: latLng,
+           map: map
+         });
+         lat = latLng.lat();
+         lon =latLng.lng();
+       }
+       else {
+         marker.setPosition(latLng);
+         lat = latLng.lat();
+         lon =latLng.lng();
+        }
+         map.panTo(latLng);
+       }
+
+
+         var searchBox = new google.maps.places.SearchBox(input);
+         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+         // Bias the SearchBox results towards current map's viewport.
+         map.addListener('bounds_changed', function() {
+           searchBox.setBounds(map.getBounds());
+         });
+
+         var markers = [];
+         // Listen for the event fired when the user selects a prediction and retrieve
+         // more details for that place.
+         searchBox.addListener('places_changed', function() {
+           var places = searchBox.getPlaces();
+
+           if (places.length == 0) {
+             return;
+           }
+
+           // Clear out the old markers.
+           markers.forEach(function(marker) {
+             marker.setMap(null);
+           });
+           markers = [];
+
+           // For each place, get the icon, name and location.
+           var bounds = new google.maps.LatLngBounds();
+           places.forEach(function(place) {
+             if (!place.geometry) {
+               console.log("Returned place contains no geometry");
+               return;
+             }
+             var icon = {
+               url: place.icon,
+               size: new google.maps.Size(71, 71),
+               origin: new google.maps.Point(0, 0),
+               anchor: new google.maps.Point(17, 34),
+               scaledSize: new google.maps.Size(25, 25)
+             };
+
+             // Create a marker for each place.
+             markers.push(new google.maps.Marker({
+               map: map,
+               icon: icon,
+               title: place.name,
+               position: place.geometry.location
+             }));
+             lat = place.geometry.location.lat();
+             lon =place.geometry.location.lng();
+             console.log(lat);
+             console.log(lon);
+             if (place.geometry.viewport) {
+               // Only geocodes have viewport.
+               bounds.union(place.geometry.viewport);
+             } else {
+               bounds.extend(place.geometry.location);
+             }
+           });
+           map.fitBounds(bounds);
+         });
+
+
+  }
+
+
 
  handleUpdate(id){
   this.id=id;
@@ -69,7 +176,7 @@ class UpdateRest extends Component {
                            "homePage": document.getElementById('homepageurl').value,
                            "fbUrl": document.getElementById('fbpageurl').value,
                            "number": document.getElementById('telephone').value,
-                           "latitude": docume0nt.getElementById('lat').value,
+                           "latitude": document.getElementById('lat').value,
                            "longitude": document.getElementById('long').value,
                            "workHours": document.getElementById('working hours').value,
                            "image": this.state.image_data
@@ -92,6 +199,15 @@ class UpdateRest extends Component {
   this.setState({get_data: event.target.value});
  }
 
+
+ getLoc(){
+   this.setState({lat1:lat});
+   this.setState({lon1:lon});
+  this.state.get_data.latitude=this.state.lat1;
+   this.state.get_data.longitude=this.state.lon1;
+   console.log(this.state.get_data.latitude);
+ }
+
  render() {
     // console.log('The App component was rendered')
     return(
@@ -109,8 +225,11 @@ class UpdateRest extends Component {
 
 
 
-<div className="container">
-      <div className="form-group row">
+       <div  className="container" >
+       <div className="row">
+       <div className="col col-sm-6">
+
+             <div className="form-group row">
          <label className="col-2 col-form-label">Name</label>
             <div className="col-6">
               <input type="text" value={this.state.get_data.name} onChange={this.handleChange} id="name"/>
@@ -181,13 +300,13 @@ class UpdateRest extends Component {
   <div className="form-group row">
     <label for="example-text-input" className="col-2 col-form-label">Latitude</label>
     <div className="col-6">
-      <input type="text" value={this.state.get_data.latitude} onChange={this.handleChange} id="lat"/>
+      <input type="text" value={this.state.get_data.latitude} id="lat"/>
     </div>
   </div>
   <div className="form-group row">
     <label for="example-text-input" className="col-2 col-form-label">Longitude</label>
     <div className="col-6">
-      <input type="text" value={this.state.get_data.longitude} onChange={this.handleChange} id="long"/>
+      <input type="text" value={this.state.get_data.longitude} id="long"/>
     </div>
   </div>
   <div className="form-group row">
@@ -206,10 +325,28 @@ class UpdateRest extends Component {
 
 
   </div>
-</div>
+
+  <div className="col col-sm-6">
+    <div className="card">
+      <div className="card-block">
+      <div>
+       <input id="pac-input" className="form-control" size="45" type="text" placeholder="Search Box"/>
+      <button type="button" className="btn btn-danger btn-sm" onClick={this.handleUpdateMap.bind(this)}>View Map</button>
+      <div id="map"  style={{height:"500px",width:"450px"}}></div>
+      <button type="button" className="btn btn-danger btn-sm" onClick={this.getLoc}>Get LatLong</button>
+
+      </div>
+      </div>
+    </div>
+  </div>
 
 
 </div>
+</div>
+</div>
+</div>
+
+
       );
   }
 }
