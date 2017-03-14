c7a44a54213e0e76ff51505f258412f88f8dad4a
bmanisha <bmanisha@Manisha-Bodapunti.local>
2017-03-14 16:35:32 +0530

    filters

--- Reactcode/src/Filters.js
+++ Reactcode/src/Filters.js
@@ -8,8 +8,8 @@ class Filters extends Component {
     super();
      console.log('jjj');
     this.state = { colltn:'', data: [],filter_data:[],check:''};
-    this.onCollChanged=this.onCollChanged.bind(this);
-        this.onInputChanged=this.onInputChanged.bind(this);
+    this.handleFilter=this.handleFilter.bind(this);
+
   }
   componentDidMount(){
 
@@ -22,21 +22,58 @@ class Filters extends Component {
              });
           });
   }
-  onCollChanged(e) {
+ handleFilter(e) {
      var that=this;
-     var cf;
-     console.log(e.currentTarget.value);
-     that.state.colltn= e.currentTarget.value;
-     console.log(that.state.colltn);
+     var cf,c1,c2,cost;
+     that.state.colltn= document.getElementById('collection').value;
+     that.state.check=document.getElementById('delivery').checked;
+     cost=document.getElementById('cost').value;
      console.log(that.state.check);
-     if(that.state.check){
+     console.log(cost);
+      if(that.state.check==true)
+          that.state.check='1';
+      else that.state.check='0';
+        if(cost==1){
+              c1=0;
+              c2=250;
+          }
+        else if(cost==2){
+                c1=250;
+                c2=500;
+        }
+        else if(cost==3){
+                c1=500;
+                c2=1000;
+        }
+        else if(cost==4){
+                c1=1000;
+                c2=2000;
+        }
+        else if(cost==5){
+                c1=2000;
+                c2=10000;
+        }
+        else{
+          c1=0;
+          c2=10000;
+        }
+
+
+     console.log(that.state.colltn);
+     if(that.state.check)
+        cf=that.props.s+"&delivery="+that.state.check;
+     if(cost>=1){
+       console.log(c1);
+        cf=that.props.s+"&cost1="+c1+"&cost2="+c2;
+      }
+     if(!that.state.colltn)
+        cf=that.props.s+"&collection="+that.state.colltn;
+     if(that.state.check&&(this.state.colltn=='1')){
        cf=that.props.s+"&collection="+that.state.colltn+"&delivery="+that.state.check;
-       console.log(cf);
+
      }
-     else {
-       cf=that.props.s+"&collection="+that.state.colltn;
-       console.log(cf);
-     }
+     console.log(cf);
+
     fetch("http://localhost:9000/filter?keyword="+cf)
     .then((response) => response.json())
      .then((responseJson) => {
@@ -51,33 +88,6 @@ class Filters extends Component {
 
   }
 
-  onInputChanged(e) {
-    var that=this;
-    var hd;
-  	that.state.check=e.currentTarget.checked;
-    if(that.state.check==true)
-        that.state.check='1';
-    else that.state.check='0';
-    console.log(that.state.check);
-    if(that.state.colltn){
-      hd="&collection="+that.state.colltn+"&delivery="+that.state.check;
-    }
-    else {
-      hd="&delivery="+that.state.check;
-    }
-    fetch("http://localhost:9000/filter?keyword="+that.props.s+hd)
-    .then((response) => response.json())
-     .then((responseJson) => {
-        that.setState({
-         filter_data: responseJson
-
-       });
-     }).then(function(e){
-               console.log(that.state.filter_data);
-               that.props.filtercoll(that.state.filter_data);
-           });
-
-  }
    render() {
       return (
          <div>
@@ -90,7 +100,7 @@ class Filters extends Component {
           <FormGroup tag="fieldset">
            <FormGroup check>
             <Label check>
-              <Input type="radio" name="radio1"  id="collection" value={data.collection}  onChange={this.onCollChanged.bind(this)}/>{' '}
+              <Input type="radio" name="radio1"  id="collection" value={data.collection}  onClick={this.handleFilter.bind(this)}/>{' '}
             {data.collection}
             </Label>
           </FormGroup>
@@ -98,7 +108,7 @@ class Filters extends Component {
         )}
       </div><hr/>
          <h5 className="card-text" style={{paddingLeft:"1px"}}> Cost </h5>
-            <select className="form-control form-control-sm" id="cost">
+            <select className="form-control form-control-sm" id="cost" onChange={this.handleFilter.bind(this)}>
                     <option selected>Cost per Two</option>
                     <option value="1">Less than &#8377;250</option>
                     <option value="2">&#8377;250 - &#8377;500</option>
@@ -109,7 +119,7 @@ class Filters extends Component {
             <hr/>
             <FormGroup check>
               <Label check>
-                <Input type="checkbox"  onClick={this.onInputChanged.bind(this)}/>{' '}
+                <Input type="checkbox" id="delivery" onClick={this.handleFilter.bind(this)}/>{' '}
                 Home Delivery
               </Label>
             </FormGroup>
