7793a87f3de4ca04e148929a7b609446daaf1f50
SaiSruthiVeluri <saisruthiveluri@gmail.com>
2017-03-16 06:33:11 +0530

    dob changes

--- Reactcode/src/UserHeader.js
+++ Reactcode/src/UserHeader.js
@@ -16,14 +16,10 @@ class UserHeader extends Component {
     // In a constructor, call `super` first if the className extends another className
 
     super();
-    this.state = { data: [] };
+    this.state = { data: [],get_data:[],popoverOpen: false,isOpen: false};
     this.handleLogout = this.handleLogout.bind(this);
     this.toggle = this.toggle.bind(this);
     this.toggle1 = this.toggle1.bind(this);
-
-    this.state = {
-      isOpen: false,data:[],  popoverOpen: false
-    };
 
     console.log('test');
 
@@ -36,23 +32,17 @@ class UserHeader extends Component {
 componentWillMount(){
 
   fetch("http://localhost:9000/images/5498f7c0-ca2f-44b4-826c-0deb07521b20")
-	.then(function(response) {
-	  return response.blob();
-	})
-	.then(function(imageBlob) {
-	  document.getElementById('logo').src = URL.createObjectURL(imageBlob);
-	});
+  .then(function(response) {
+    return response.blob();
+  })
+  .then(function(imageBlob) {
+    document.getElementById('logo').src = URL.createObjectURL(imageBlob);
+  });
 
   var tok=window.sessionStorage.getItem('token');
    var id=window.sessionStorage.getItem('uid');
-  fetch("http://localhost:9000/members/"+id,
-  {
-    headers :{
-    "Content-Type" : "application/json",
-    "Authorization": "Bearer "+tok
-  }
-}
- ).then((response) => response.json())
+  fetch("http://localhost:9000/members/"+id)
+  .then((response) => response.json())
        .then((responseJson) => {
           this.setState({
            get_data: responseJson
@@ -96,9 +86,8 @@ componentWillMount(){
      method: "PUT",
      body: JSON.stringify({
                             "email": document.getElementById('email').value,
-                            "pwd": document.getElementById('pwd').value,
-                            "bday": document.getElementById('bday').value,
-                            "address": document.getElementById('address').value,
+                            "dob": document.getElementById('bday').value,
+                            "user_Address": document.getElementById('address').value,
                           })
     }).then(response=>{
       if(200==response.status){
@@ -125,7 +114,7 @@ return(
       <Navbar fixed="top" toggleable>
           <NavbarToggler right onClick={this.toggle1}><i className="fa fa-bars fa-2x" aria-hidden="true"></i></NavbarToggler>
           <NavbarBrand href="/">
-          <img id="logo" width="60" height="60" className="d-inline-block align-center" />Find'O Bistro</NavbarBrand>
+          <img id="logo" width="60" height="60" className="d-inline-block align-center" />FindO Bistro</NavbarBrand>
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
@@ -135,30 +124,24 @@ return(
                 <NavLink href="#tf-collection"><b>Collections</b></NavLink>
               </NavItem>
               <NavItem>
-                <Button id="Popover1" onClick={this.toggle} >
-                  <i className="fa fa-user-circle-o" aria-hidden="true"></i>
+                <Button id="Popover1" style={{backgroundColor:"transparent",borderColor:"transparent"}}  onClick={this.toggle} >
+                  <i  className="fa fa-user-circle-o" aria-hidden="true"></i>
                 </Button>
                 <Popover placement="bottom right" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
-                  <PopoverTitle>My Profile
-                    <div style={{color:"#000000",float:"right"}}><button type="button" id="logout1" className="btn btn-secondary" onClick={this.handleLogout}><i className="fa fa-sign-out" aria-hidden="true"></i></button></div>
+                  <PopoverTitle>
+                  My Profile
                   </PopoverTitle>
                   <PopoverContent>
-                    <div className="w3-container">
+                    <div >
 
                       <p className="w3-center"><img src={require('./images/avatar3.png')} className="w3-circle" style={{height:"106px",width:"106px"}} alt="Avatar"/></p>
 
-                      <p style={{position:"align-center"}}>{window.sessionStorage.getItem('name')}</p>
-                      <hr/>
-                      <div className="form-group row">
-                        <label for="example-text-input" className="col-2 col-form-label"><i className="fa fa-key" aria-hidden="true"></i></label>
-                        <div className="col-8">
-                           <input type="text" className="form-control" /*value={this.state.get_data.pwd}*/ onChange={this.handleChange} id="pwd"/>
-                        </div>
-                      </div>
+                      <p className="w3-center">{this.state.get_data.name}</p>
+                      <hr/>        
                       <div className="form-group row">
                         <label for="example-text-input" className="col-2 col-form-label"><i className="fa fa-envelope-o" aria-hidden="true"></i></label>
                         <div className="col-8">
-                           <input type="text" className="form-control" /*value={this.state.get_data.email}*/ onChange={this.handleChange} id="email"/>
+                           <input type="text" className="form-control" value={this.state.get_data.email} onChange={this.handleChange} id="email"/>
                         </div>
                       </div>
                       <div className="form-group row">
@@ -178,9 +161,13 @@ return(
                            <button type="button" className="btn btn-warning" onClick={() => this.handleUpdate(this.state.get_data.id)}>Submit</button>
                          </div>
                      </div>
+                      <div className="form-group-row">
+                         <div className="col-8">
+                            <button type="button" id="logout1" className="btn btn-secondary" onClick={this.handleLogout}><i className="fa fa-sign-out" aria-hidden="true">LOGOUT</i></button>
+                          </div>
+                     </div>
 
                     </div>
-
                   <br/>
                   </PopoverContent>
                 </Popover>
--- Reactcode/src/UserHome.js
+++ Reactcode/src/UserHome.js
@@ -17,7 +17,28 @@ class UserHome extends Component {
     super();
     this.state = { data: [] };
   }
-
+  componentDidMount(){
+     var tok=window.sessionStorage.getItem('token');
+    fetch("http://localhost:9000/users/users_dob",
+     {
+       headers :{
+         "Content-Type" : "application/json",
+         "Authorization": "Bearer "+tok
+       }
+    }).then((response) => response.json())
+         .then((responseJson) => {
+            this.setState({
+             data: responseJson
+           });
+         }).then(function(e){
+     document.getElementById('bday').style.display="block";
+    });
+  }
+  handleRest(id)
+{
+ this.id=id;
+ hashHistory.push('/Restaurant_detail/'+id)
+}
      render() {
        console.log(window.sessionStorage.getItem('token'));
       return (
@@ -31,9 +52,32 @@ class UserHome extends Component {
              </div>
          </div>
          <div id="ss">
-         <Collection />
-
-         <div className="text-center"><a href="#tf-home" className="fa fa-angle-up fa-3x"></a></div>
+            <div id="bday" classNAme="row" style={{display: 'none'}}>
+              <div className="col"><i style={{padding:"50px 200px 50px 50px",fontSize:"22px",float:"left"}}>We Found a Best place to Celebrate your Birthday </i></div>
+              
+              <div className="col">  
+                <div id="srchcard"  className="card w-100">
+                   <div className="row" id="srch">
+                     <div className="col-md-6">
+                       <img id="srchimg" src={this.state.data.image} id="srchimg" alt="Card image cap"/>
+                     </div>
+                     <div className="col-md-6" id="srchcard1">
+                         <div className="card-top">
+                           <h5 className="card-subtitle">{this.state.data.name}</h5>
+                           <p className="card-text"> Area: {this.state.data.area}</p>
+                           <p className="card-text">  Working Hours: {this.state.data.workHours}</p>
+                         </div><hr/>
+                         <div className="card-bottom" id="srch">
+                           <button type="button" className="btn btn-warning btn-sm"  style={{float:"right"}} onClick={()=>this.handleRest(this.state.data.id)}>View</button>
+                         </div>
+                     </div>
+                   </div>
+                </div>
+                <hr />                
+             </div>
+             </div>
+            <Collection />
+            <div className="text-center"><a href="#tf-home" className="fa fa-angle-up fa-3x"></a></div>
          </div>
          <Footer />
          </div>
